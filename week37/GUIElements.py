0adf46a1a2b91d08667be17eda391debd5a2794d
bradur <bradurdung@gmail.com>
2013-09-26 22:51:01 +0300

    Rewrote GUI.

---
+++ week37/GUIElements.py
@@ -0,0 +1,281 @@
+# -*- coding: utf-8 -*-
+import pygame
+
+
+class GUIElement(pygame.sprite.Sprite):
+    def __init__(self, style, screensize, text, align):
+        pygame.sprite.Sprite.__init__(self)
+
+        # padding and border size
+        self.padding = screensize[0]/100
+        self.bordersize = screensize[0]/200
+        self.fullpadding = self.padding*2+self.bordersize*2
+
+        self.align = align
+        self.screensize = screensize
+        self.text = text
+        self.style = style
+        self.color = style["color_text"]
+
+        # get font values
+        font_family = style["font_family"]
+        font_size = style["font_size"]
+
+        # distinguish between system fonts and font files
+        if style["font_type"] == "custom":
+            self.font = pygame.font.Font(font_family, font_size)
+        elif style["font_type"] == "system":
+            try:
+                self.font = pygame.font.SysFont(font_family, font_size)
+            except:
+                print "Font not found, defaulting to Arial..."
+                self.font = pygame.font.Font("Arial", font_size)
+
+        self.text_image = self.font.render(text, True, self.color)
+        self.rect = pygame.Rect(
+            0, 0,
+            self.text_image.get_width()+self.fullpadding,
+            self.text_image.get_height()+self.fullpadding
+        )
+
+        self.resize(self.rect.width, self.rect.height)
+
+    def resize(self, w, h):
+
+        self.rect.width, self.rect.height = w+self.padding, h+self.padding
+
+        t = self.padding
+        l = self.padding
+        b = self.screensize[1]-self.rect.height-self.padding
+        r = self.screensize[0]-self.rect.width-self.fullpadding-self.padding
+        c = self.screensize[0]/2-self.rect.width/2
+        m = self.screensize[1]/2-self.rect.height/2
+
+        aligns = {
+            "left": (l, m),
+            "right": (r, m),
+            "top": (c, t),
+            "topleft": (l, t),
+            "topright": (r, t),
+            "bottom": (c, b),
+            "bottomleft": (l, b),
+            "bottomright": (r, b),
+            "center": (c, m)
+        }
+
+        self.rect.x, self.rect.y = aligns[self.align]
+
+        self.image = pygame.Surface((self.rect.width, self.rect.height))
+        self.image.convert()
+
+        # border color
+        self.border = pygame.Surface((self.rect.width, self.rect.height))
+        self.border.convert()
+        self.border.fill(self.style["color_border"])
+        self.image.blit(self.border, (0, 0))
+
+        # background color
+        self.background = pygame.Surface((
+            self.rect.width-self.bordersize*2,
+            self.rect.height-self.bordersize*2
+        ))
+        self.background.convert()
+        self.background.fill(self.style["background_default"])
+        self.image.blit(self.background, (self.bordersize, self.bordersize))
+        w, h = self.text_image.get_size()
+        self.image.blit(
+            self.text_image,
+            (self.rect.width/2-w/2, self.rect.height/2-h/2)
+        )
+
+
+class ScoreDisplay(GUIElement):
+    def __init__(self, style, screensize, text, align):
+        super(ScoreDisplay, self).__init__(style, screensize, text, align)
+
+        self.score = 0
+        h = self.rect.height
+        self.resize(screensize[0]-self.bordersize*2, h*2-self.padding*2)
+        
+
+        self.scores = [
+            0,
+            100,
+            200,
+            300,
+            500,
+            1000,
+            2000,
+            4000,
+            8000,
+            16000,
+            32000,
+            64000,
+            125000,
+            250000,
+            500000,
+            1000000
+        ]
+
+        self.update()
+
+    def update(self):
+
+        text = self.text+str(self.scores[self.score])
+
+        self.image.blit(self.border, (0, 0))
+        self.image.blit(self.background, (self.bordersize, self.bordersize))
+        self.text_image = self.font.render(text, True, self.color)
+
+        w, h = self.text_image.get_size()
+        imagepos = (self.rect.width/2-w/2, self.rect.height/2-h/2)
+
+        self.image.blit(self.text_image, imagepos)
+        if self.score < len(self.scores):
+            self.score += 1
+
+    def getscore(self):
+        return self.scores[self.score]
+
+    def prevscore(self):
+        return self.scores[self.score-1]
+
+class Popup(GUIElement):
+    def __init__(self, style, screensize, text, align, size=None, fade=True,
+                 float=False):
+        super(Popup, self).__init__(style, screensize, text, align)
+
+        self.alpha = 255
+
+        self.fade = fade
+        self.float = float
+        self._start = pygame.time.get_ticks()
+        self._delay = 500
+        self._fdelay = 100
+        self._last_update = 0
+
+        if size is not None:
+            if size[0] > self.rect.width:
+                self.rect.width = size[0]
+            if size[1] > self.rect.height:
+                self.rect.height = size[1]
+            self.resize(self.rect.width, self.rect.height)
+
+        self.update(pygame.time.get_ticks())
+
+    def update(self, time):
+
+        if self.fade and time - self._last_update-self._start > self._delay:
+            if self.alpha > 0:
+                self.alpha -= 5
+            self.image.set_alpha(self.alpha)
+        if self.float and time - self._last_update-self._start > self._fdelay:
+            self.rect.y -= 1
+
+
+class QuestionDisplay(GUIElement):
+    def __init__(self, style, screensize, text, align):
+        super(QuestionDisplay, self).__init__(style, screensize, text, align)
+
+        size = self.text_image.get_size()
+        if size[0] >= screensize[0]-self.fullpadding-self.padding*2:
+            divider = size[0]/(screensize[0]-self.fullpadding-self.padding*2)+1
+            slist = []
+
+            part = len(text)/divider
+            while True:
+                t = text[:part]
+                if part > len(text):
+                    slist.append(text)
+                    break
+                else:
+                    splitter = t.rfind(" ")
+                    slist.append(text[:splitter])
+                    text = text[splitter:]
+
+            #for i in xrange(0, len(tlist), len(tlist)/divider):
+            #    slist.append(" ".join(tlist[i:i+len(tlist)/divider]))
+
+            size = self.font.size(slist[0])
+            for item in slist:
+                sz = self.font.size(item)
+                if size[0] < sz[0]:
+                    size = sz
+            self.text_image = pygame.Surface((
+                size[0]+self.padding*2,
+                size[1]*len(slist)+self.padding*(len(slist)-1)
+            ))
+            self.text_image.fill(style["background_default"])
+            for i, item in enumerate(slist):
+                self.text_image.blit(self.font.render(
+                    item, True, self.color),
+                    (self.padding, i*size[1]+i*self.padding))
+            size = self.text_image.get_width(), self.text_image.get_height()
+
+        else:
+            size = (size[0]+self.padding*2, size[1])
+
+        self.resize(size[0], size[1])
+
+
+class Button(GUIElement):
+    def __init__(self, style, screensize, text, align, action, id=None):
+        super(Button, self).__init__(style, screensize, text, align)
+
+        # states
+        self.colorchange = None
+        self.hovered = False
+        self.action = action
+
+        choicesize_x = screensize[0]/2-self.padding*3-self.fullpadding*3
+        buttonsize_x = screensize[0]/4
+
+        sizes = [
+            [choicesize_x, self.rect.height-self.fullpadding],
+            [choicesize_x, self.rect.height-self.fullpadding],
+            [choicesize_x, self.rect.height-self.fullpadding],
+            [choicesize_x, self.rect.height-self.fullpadding],
+            [buttonsize_x, self.rect.height-self.fullpadding],
+            [buttonsize_x, self.rect.height-self.fullpadding]
+        ]
+
+        self.resize(sizes[id][0], sizes[id][1])
+
+        if id is not None:
+            positions = [
+                [self.padding,
+                 screensize[1]-self.rect.height*2-self.padding*2],
+                [self.padding, screensize[1]-self.rect.height-self.padding],
+                [screensize[0]-self.rect.width-self.padding,
+                 screensize[1]-self.rect.height*2-self.padding*2],
+                [screensize[0]-self.rect.width-self.padding,
+                 screensize[1]-self.rect.height-self.padding],
+                [screensize[0]/2-self.rect.width/2,
+                 screensize[1]/2-self.rect.height*2],
+                [screensize[0]/2-self.rect.width/2,
+                 screensize[1]/2+self.rect.height]
+            ]
+
+            self.rect.x, self.rect.y = positions[id]
+
+    def update(self):
+
+        if self.colorchange:
+            self.background.fill(self.style[self.colorchange])
+            self.colorchange = None
+
+        self.image.blit(self.background, (self.bordersize, self.bordersize))
+
+        w, h = self.text_image.get_size()
+        imagepos = (self.rect.width/2-w/2, self.rect.height/2-h/2)
+        self.image.blit(self.text_image, imagepos)
+
+    def unhover(self):
+        self.colorchange = "background_default"
+        self.hovered = False
+        self.update()
+
+    def hover(self):
+        self.colorchange = "background_hover"
+        self.hovered = True
+        self.update()
--- week37/game.py
+++ week37/game.py
@@ -3,7 +3,7 @@ import pygame
 import json
 import os
 from pygame.locals import QUIT, MOUSEBUTTONDOWN, KEYDOWN, K_ESCAPE
-from classes import Popup, Button, QuestionDisplay, ScoreDisplay
+from GUIElements import ScoreDisplay, QuestionDisplay, Popup, Button
 
 
 class Game(object):
@@ -20,7 +20,7 @@ class Game(object):
         info = pygame.display.Info()            # get display info
         screen_height = info.current_h
         screen_width = info.current_w
-        self.width, self.height = 800, 800
+        self.width, self.height = 700, 700
         if screen_height < 800:
             self.height = 600
             self.width = 600
@@ -38,79 +38,100 @@ class Game(object):
         self.successound = pygame.mixer.Sound("success.ogg")
 
         self.fps = 60
-        image = pygame.image.load("snake.png")
-        image.convert_alpha()
-        image = pygame.transform.smoothscale(image, (self.height, self.height))
+        self.snakeimage = pygame.image.load("snake.png")
+        self.snakeimage.convert_alpha()
+        self.snakeimage = pygame.transform.smoothscale(
+            self.snakeimage,
+            (self.height, self.height)
+        )
         self.background = pygame.Surface((self.width, self.height))
-        self.background.fill((255, 255, 255))
-        self.background.blit(image, (0, 0))
+
         self.overlay = pygame.Surface((self.width, self.height))
         self.overlay.convert_alpha()
         self.overlay.fill((0, 0, 0))
-        self.overlay.set_alpha(150)
+        self.overlay.set_alpha(130)
 
         self.show_overlay = False
-        self.background.convert()
+        self.show_GUI = False
 
-        self.choice_font = {
-            "type": "custom",
-            "family": "ArchivoBlack-Regular.otf",
-            "size": self.width/30
-        }
-
-        self.choice_colors = {
-            "text": [255, 255, 255],
-            "background": [65, 44, 132],
-            "hover": [135, 110, 215],
-            "border": [32, 7, 114]
+        self.choice_style = {
+            "font_type": "custom",
+            "font_family": "ArchivoBlack-Regular.otf",
+            "font_size": self.width/30,
+            "color_text": [30, 30, 31],
+            "background_default": [203, 209, 207],  # 2EA736
+            "background_hover": [253, 227, 44],     # 08DE16
+            "color_border": [240, 240, 240]          # 03900C
         }
 
         self.choices = pygame.sprite.Group()
 
-        self.question_font = {
-            "type": "custom",
-            "family": "Aleo-Regular.otf",
-            "size": self.width/20
-        }
-
-        self.question_colors = {
-            "text": [255, 255, 255],
-            "background": [65, 44, 132],
-            "border": [32, 7, 114]
+        self.question_style = {
+            "font_type": "custom",
+            "font_family": "Aleo-Regular.otf",
+            "font_size": self.width/20,
+            "color_text": [255, 255, 255],
+            "background_default": [46, 167, 54],  # 466A58
+            "color_border": [112, 239, 120]          # 30493D
         }
 
         self.question = pygame.sprite.GroupSingle()
 
-        self.score_colors = {
-            "text": [0, 0, 0],
-            "background": [255, 255, 255],
-            "border": [240, 240, 240]
-        }
-
-        self.popup_font = {
-            "type": "custom",
-            "family": "ArchivoBlack-Regular.otf",
-            "size": self.width/10
-        }
-
-        self.popup_colors_right = {
-            "text": [255, 255, 255],
-            "background": [38, 153, 38],
-            "hover": [131, 224, 131],
-            "border": [0, 133, 0]
-        }
-
-        self.popup_colors_wrong = {
-            "text": [255, 255, 255],
-            "background": [191, 48, 48],
-            "hover": [223, 139, 139],
-            "border": [255, 64, 64]
+        self.score_style = {
+            "font_type": "custom",
+            "font_family": "ArchivoBlack-Regular.otf",
+            "font_size": self.width/30,
+            "color_text": [0, 0, 0],
+            "background_default": [255, 255, 255],
+            "color_border": [240, 240, 240]
         }
 
         self.scoredisplay = pygame.sprite.GroupSingle()
+
+        self.button_style_play = {
+            "font_type": "custom",
+            "font_family": "ArchivoBlack-Regular.otf",
+            "font_size": self.width/30,
+            "color_text": [255, 255, 255],
+            "background_default": [38, 153, 38],
+            "background_hover": [131, 224, 131],
+            "color_border": [0, 133, 0]
+        }
+
+        self.button_style_quit = {
+            "font_type": "custom",
+            "font_family": "ArchivoBlack-Regular.otf",
+            "font_size": self.width/30,
+            "color_text": [255, 255, 255],
+            "background_default": [191, 48, 48],
+            "background_hover": [223, 139, 139],
+            "color_border": [255, 64, 64]
+        }
+
+        self.popup_style_right = {
+            "font_type": "custom",
+            "font_family": "ArchivoBlack-Regular.otf",
+            "font_size": self.width/10,
+            "color_text": [255, 255, 255],
+            "background_default": [46, 167, 54],  # 2EA736
+            "background_hover": [8, 222, 22],     # 08DE16
+            "color_border": [3, 144, 12]          # 03900C
+        }
+
+        self.popup_style_wrong = {
+            "font_type": "custom",
+            "font_family": "ArchivoBlack-Regular.otf",
+            "font_size": self.width/10,
+            "color_text": [255, 255, 255],
+            "background_default": [191, 48, 48],
+            "background_hover": [223, 139, 139],
+            "color_border": [255, 64, 64]
+        }
+
         self.buttons = pygame.sprite.Group()
 
         self.popup = pygame.sprite.GroupSingle()
+        self.topinfo = pygame.sprite.GroupSingle()
 
         self.title = pygame.sprite.GroupSingle()
 
@@ -120,21 +141,26 @@ class Game(object):
         self.start_game()
 
     def start_game(self):
+        self.background.fill((255, 255, 255))
+        self.background.blit(self.snakeimage, (0, 0))
+        self.background.convert()
+
         with open('qa.json') as data_file:
             self.questions_answers = json.load(data_file)["qas"]
 
         self.scoredisplay.add(ScoreDisplay(
-            self.question_font,
-            self.score_colors,
+            self.score_style,
             [self.width, self.height],
-            pygame.Rect(0, 0, 0, 0),
-            "/{0}".format(len(self.questions_answers)),
-            [False, False, False, False],
+            u"\u00A3",
+            #"/{0}".format(len(self.questions_answers)),
+            "bottom"
         ))
+        self.scoredisplay.sprite.rect.bottom = self.height
         self.popup.empty()
         self.buttons.empty()
         self.next_question()
         self.show_overlay = False
+        self.show_GUI = True
 
     def quit_game(self):
         return False
@@ -144,85 +170,73 @@ class Game(object):
         while True:
             if self.input() is False:
                 return False
-            self.choices.update(pygame.time.get_ticks())
             if self.popup.sprite:
                 self.popup.update(pygame.time.get_ticks())
-            if self.buttons:
-                self.buttons.update(pygame.time.get_ticks())
-            #self.spritegroup.update(pygame.time.get_ticks())
+            if self.topinfo.sprite:
+                self.topinfo.update(pygame.time.get_ticks())
             self.draw()
-
-    def show_popup(self, text, colors):
-        self.popup.add(Popup(
-            self.popup_font,
-            colors,
-            [self.width, self.height],
-            pygame.Rect(0, 0, self.width/2, self.height/2),
-            text,
-            [True, True, True, True]
-        ))
 
     def next_question(self):
         if len(self.questions_answers) > 0:
             self.qac = self.questions_answers.pop(0)
 
             self.question.add(QuestionDisplay(
-                self.question_font,
-                self.question_colors,
+                self.question_style,
                 [self.width, self.height],
-                pygame.Rect(0, self.height/10, 0, 0),
                 self.qac["question"],
-                [True, False, False, False],
-                self.qac["answer"]
+                "top"
             ))
 
             self.choices.empty()
             for index, choice in enumerate(self.qac["choices"]):
                 self.choices.add(Button(
-                    self.choice_font,
-                    self.choice_colors,
+                    self.choice_style,
                     [self.width, self.height],
-                    pygame.Rect(0, 0, 0, 0),
                     choice,
-                    [False, False, False, False],
+                    "bottom",
                     "",
-                    id=index+1
+                    id=index
                 ))
 
         else:
             self.popup.add(Popup(
-                self.popup_font,
-                self.choice_colors,
+                self.score_style,
                 [self.width, self.height],
-                pygame.Rect(0, 0, self.width/2, self.height/2),
                 "Game Over!",
-                [True, True, True, False],
-                fade=False
+                "top",
+                fade=False,
+                size=[self.width/2, 0]
+            ))
+
+            score = self.scoredisplay.sprite.prevscore()
+            self.topinfo.add(Popup(
+                self.score_style,
+                [self.width, self.height],
+                "You made "+u"\u00A3"+str(score)+"!",
+                "center",
+                fade=False,
+                size=[self.width/2, self.height/3]
             ))
 
             self.show_overlay = True
-            y = self.popup.sprite.rect.bottom
+            self.show_GUI = False
 
             self.buttons.add(Button(
-                self.choice_font,
-                self.popup_colors_right,
+                self.button_style_play,
                 [self.width, self.height],
-                pygame.Rect(0, y, 0, 0),
                 "PLAY AGAIN",
-                [True, False, False, False],
+                "bottom",
                 "start_game",
-                id=5
+                id=4
             ))
 
             self.buttons.add(Button(
-                self.choice_font,
-                self.popup_colors_wrong,
+                self.button_style_quit,
                 [self.width, self.height],
-                pygame.Rect(0, y, 0, 0),
                 "QUIT GAME",
-                [True, False, False, False],
+                "bottom",
                 "quit_game",
-                id=6
+                id=5
             ))
 
     def input(self):
@@ -234,15 +248,15 @@ class Game(object):
                     if not self.show_overlay:
                         choice.hover()
                     else:
-                        choice.normal()
+                        choice.unhover()
                 else:
-                    choice.normal()
+                    choice.unhover()
 
             for button in self.buttons:
                 if pygame.sprite.spritecollide(button, self.mouse, False):
                     button.hover()
                 else:
-                    button.normal()
+                    button.unhover()
 
             if event.type == QUIT:
                 return False
@@ -267,23 +281,51 @@ class Game(object):
             return False
 
     def check_answer(self, text):
-        answer = self.question.sprite.check_answer(text)
-        if answer:
-            self.show_popup("Correct!", self.popup_colors_right)
+
+        if self.qac["answer"] == text:
+            newscore = self.scoredisplay.sprite.getscore()
+            oldscore = self.scoredisplay.sprite.prevscore()
+            difference = newscore-oldscore
+
+            self.topinfo.add(Popup(
+                self.button_style_play,
+                [self.width, self.height],
+                u"\u00A3"+str(difference),
+                "bottom",
+                float=True
+            ))
+
             self.scoredisplay.update()
             self.successound.play()
         else:
-            self.show_popup("Wrong!", self.popup_colors_wrong)
+            self.topinfo.add(Popup(
+                self.button_style_quit,
+                [self.width, self.height],
+                self.qac["answer"],
+                "bottom"
+            ))
             self.failsound.play()
         self.next_question()
 
     def draw(self):
-        self.screen.blit(self.background, (0, 0))
-        self.scoredisplay.draw(self.screen)
-        self.question.draw(self.screen)
-        self.choices.draw(self.screen)
+
         if self.show_overlay:
+            self.scoredisplay.draw(self.screen)
+            self.question.draw(self.screen)
+            self.choices.draw(self.screen)
             self.screen.blit(self.overlay, (0, 0))
+            self.background.blit(self.screen, (0, 0))
+            self.show_overlay = False
+
+        self.screen.blit(self.background, (0, 0))
+
+        if self.show_GUI:
+            self.scoredisplay.draw(self.screen)
+            self.question.draw(self.screen)
+            self.choices.draw(self.screen)
+
+        self.topinfo.draw(self.screen)
+
         self.popup.draw(self.screen)
         self.buttons.draw(self.screen)
         pygame.display.update()
